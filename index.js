323b6a53c1645ad824ee60801e000fda9604e0cd
eman hamid <emanhharki@gmail.com>
2021-04-25 16:51:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// let num1 = 2;
+// let num2 = 31;
+// let result = num1 * num2;
+// console.log(result);
+
+
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply);
+
+// let random =
+let mod = num3 % num4;
+let num3 = 14;
+let num4 =5;
+console.log(mod);
